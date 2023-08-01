91b272a1da6e435a57c02af6a9db0c1a50d24f5b
Ajay Singh <ajaythesmart27@gmail.com>
2023-08-01 17:09:56 +0530

    Resume updated

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,7 +10,7 @@ const index=()=> {
   
   return (
     <motion.div className='flex flex-col flex-grow px-6 pt-1' variants={routeAnimation} initial='initial' animate='animate' exit='exit'>
-      <h5 className='my-3 font-semibold text-white font-Hanken'>I am currently pursuing B.Tech Degree (2nd year) in Computer Science Engineering from IIITM Gwalior.<br/>I am Full Stack Developer based in India and build dynamic data-driven websites and applications using a rich ensemble of technologies and techniques.</h5>
+      <h5 className='my-3 font-semibold text-white font-Hanken'>I am currently pursuing B.Tech Degree (3rd year) in Computer Science Engineering from IIITM Gwalior.<br/>I am Full Stack Developer based in India and build dynamic data-driven websites and applications using a rich ensemble of technologies and techniques.</h5>
       
       <div className='flex-grow p-4 mt-4 dark:bg-extblue-500 bg-dark-100 ' style={{marginLeft:'-1.5rem',marginRight:'-1.5rem'}}>
         <h6 className='my-3 text-2xl font-medium tracking-wide text-white dark:text-white font-Acme'>What i am doing currently</h6>
